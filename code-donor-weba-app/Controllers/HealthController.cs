41cc5e77a63aadf013c67941b55c2d9444aba4e1
Anil Bhasi <anil.bhasi@amnhealthcare.com>
2024-07-09 23:24:29 +0530

    health

---
+++ code-donor-weba-app/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace code_donor_weba_app.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthController:ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("Healthy");
+        }
+    }
+ }
